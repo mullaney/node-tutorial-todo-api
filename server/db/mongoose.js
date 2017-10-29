db94dfe19f29d217c5b6eb1dfa78087cbc79fd9c
Kevin Kelly <kvinklly@gmail.com>
2017-10-29 09:03:16 -0500

    altered mongoose connect

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,15 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.evn.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+var uristring =
+  process.env.MONGODB_URI ||
+  'mongodb://localhost/HelloMongoose';
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 
 module.exports = { mongoose };
