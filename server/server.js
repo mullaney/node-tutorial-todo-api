ff92d6a335b87eb7ec3756699829198e1137e612
Kevin Kelly <kvinklly@gmail.com>
2017-10-30 09:58:04 -0500

    add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const jwt        = require('jsonwebtoken');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -92,6 +93,10 @@ app.patch('/todos/:id', (req, res) => {
     res.status(400).send();
   })
 })
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
 
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
