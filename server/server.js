18acc0441a42009c88ad0f581bd8236b5bcd5728
Kevin Kelly <kvinklly@gmail.com>
2017-10-30 09:34:04 -0500

    add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const _          = require('lodash');
 const express    = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const jwt        = require('jsonwebtoken');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -91,6 +92,19 @@ app.patch('/todos/:id', (req, res) => {
     res.status(400).send();
   })
 })
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Started on port ${process.env.PORT}`);
